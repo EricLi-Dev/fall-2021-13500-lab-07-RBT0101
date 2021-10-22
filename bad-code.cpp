b188f88eec22ddb55c15353c4b2a75ee0b00402c
System Administrator <root@Zhipeng.home>
2021-10-22 00:01:14 -0400

    Modified for new changes

--- bad-code.cpp
+++ bad-code.cpp
@@ -7,10 +7,18 @@ int x = 1;
 	if (1 > 2){
 			//This never happens
 		}
-	else{ //0 > everything}
+	else{std::cout << "I like 0" << std::endl;}
+	
+	if (5>4){if (4>3){
+		if (3>2){
+			if (2 > 1){}
+		}
+			}
+	else if (9 > 5){}
 	
     for(int i = 0; i < 10; i++) {
 cout << i;
           cout << endl;
  }
+    }
     }
